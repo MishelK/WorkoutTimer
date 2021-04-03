efa572d35f1f640eb6332f2f9e418284f008f0c7
Mishel Kvit <65822657+MishelK@users.noreply.github.com>
2021-04-03 23:56:01 +0300

    Init Spotify api
    - Basic quick-start with the spotify api
    - achieved connection to spotify app
    - implemented onConnectSpotify method that sets a listener to the spotify player api

--- app/src/main/java/com/example/workouttimer/MainActivity.java
+++ app/src/main/java/com/example/workouttimer/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.workouttimer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.types.Track;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "008364546c024db1818a96724881f3e7";
+    private static final String REDIRECT_URI = "workouttimer://callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
+    TextView tv_song_artist, tv_song_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_song_artist = findViewById(R.id.tv_song_artist);
+        tv_song_name = findViewById(R.id.tv_song_name);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Connecting to spotify
+        // Setting the connection parameters
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.i("MainActivity", "Connected to spotify");
+
+                        // Interacting with app remote
+                        onConnectSpotify();
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.i("MainActivity", throwable.getMessage(), throwable);
+                    }
+                });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    private void onConnectSpotify() {
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+                        tv_song_name.setText(track.name);
+                        tv_song_artist.setText(track.artist.name);
+                    }
+                });
+    }
+
+
 }
