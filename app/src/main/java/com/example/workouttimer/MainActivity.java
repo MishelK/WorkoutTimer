accdb7ab0b3963ea8c84ec3824f19623fb5cb0e1
“MishelK” <65822657+MishelK@users.noreply.github.com>
2021-04-21 01:48:38 +0300

    Main ui updates
    - Created spotify player cardview with custom buttons
    - Implemented a callback listener to spotify api updates that updates the song details and image whenever a song in switched
    - UI theme makeover

--- app/src/main/java/com/example/workouttimer/MainActivity.java
+++ app/src/main/java/com/example/workouttimer/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import com.bumptech.glide.Glide;
 import com.example.workouttimer.services.TimerService;
 import com.example.workouttimer.utilities.Config;
 import com.mikhaellopez.circularprogressbar.CircularProgressBar;
@@ -11,6 +12,8 @@ import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 
+import com.spotify.protocol.client.CallResult;
+import com.spotify.protocol.types.Image;
 import com.spotify.protocol.types.Track;
 
 import android.content.BroadcastReceiver;
@@ -18,10 +21,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,11 +36,13 @@ public class MainActivity extends AppCompatActivity {
 
     long initialTime, remainingTime;
     float progress;
-    boolean isRunning, isReset, isFinished = false;
+    boolean isRunning, isReset, isFinished, isPlaying = false;
 
-    TextView time_tv;
     CircularProgressBar circularProgressBar;
-    Button btn_start_stop, btn_reset, btn_plus, btn_minus;
+    Button btn_start_stop, btn_reset, btn_plus, btn_minus; // Timer Buttons
+    Button btn_prev, btn_pause_play, btn_next; // Spotify Buttons
+    TextView time_tv, song_name_tv, song_artist_tv;
+    ImageView song_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +85,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Init spotify player UI
         initSpotifyPlayerBtns();
+        song_name_tv = findViewById(R.id.tv_song_name);
+        song_artist_tv = findViewById(R.id.tv_song_artist);
+        song_image = findViewById(R.id.song_iv);
 
     }
 
@@ -135,8 +145,26 @@ public class MainActivity extends AppCompatActivity {
                 .setEventCallback(playerState -> {
                     final Track track = playerState.track;
                     if (track != null) {
-                        Log.d("MainActivity", track.name + " by " + track.artist.name);
-                        // TODO: UPDATE UI
+
+                        // Setting player state
+                        isPlaying = !playerState.isPaused;
+                        if (isPlaying)
+                            btn_pause_play.setBackgroundResource(R.drawable.ic_pause);
+                        else
+                            btn_pause_play.setBackgroundResource(R.drawable.ic_play);
+                        song_name_tv.setText(track.name);
+                        song_artist_tv.setText(track.artist.name);
+
+                        // Setting current track image
+                        mSpotifyAppRemote
+                                .getImagesApi()
+                                .getImage(playerState.track.imageUri, Image.Dimension.MEDIUM)
+                                .setResultCallback(new CallResult.ResultCallback<Bitmap>() {
+                                    @Override
+                                    public void onResult(Bitmap bitmap) {
+                                        song_image.setImageBitmap(bitmap);
+                                    }
+                                });
                     }
                 });
     }
@@ -209,23 +237,18 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initSpotifyPlayerBtns (){
-        Button btn_prev, btn_pause, btn_resume, btn_next;
 
-        btn_pause = findViewById(R.id.player_pause);
-        btn_resume = findViewById(R.id.player_play);
+        btn_pause_play = findViewById(R.id.player_play_pause);
         btn_prev = findViewById(R.id.player_prev);
         btn_next = findViewById(R.id.player_next);
 
-        btn_pause.setOnClickListener(new View.OnClickListener() {
+        btn_pause_play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mSpotifyAppRemote.getPlayerApi().pause();
-            }
-        });
-        btn_resume.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mSpotifyAppRemote.getPlayerApi().resume();
+                if (isPlaying)
+                    mSpotifyAppRemote.getPlayerApi().pause();
+                else
+                    mSpotifyAppRemote.getPlayerApi().resume();
             }
         });
         btn_prev.setOnClickListener(new View.OnClickListener() {
